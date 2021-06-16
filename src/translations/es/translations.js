aa974dee347b6c87805104197c4bdc4f19d5f1cd
Michelle <mliuu3259@gmail.com>
2021-06-16 15:04:01 -0400

    added language support for de, es, fr, zh

---
+++ src/translations/es/translations.js
@@ -0,0 +1,17 @@
+export const TRANSLATIONS_ES = {
+  "title": "Pasantías de MOCTO CIC",
+  "team": "Nuestro equipo",
+  "contact": "Contáctenos",
+  "swe": "Pasante de ingeniero de software",
+  "pm": "Pasante de gerente de producto",
+  "michelle": "Michelle Liu",
+  "kara": "Kara Siegel",
+  "saiful": "Saiful Islam",
+  "anya": "Anya Dunaif",
+  "britney": "Britney Johnson",
+  "headshot": "Foto de cabeza de {{nombre}}",
+  "full_name": "Nombre completo",
+  "subject": "Asunto",
+  "email": "Correo electrónico",
+  "message": "Mensaje"
+};
--- src/translations/i18n.js
+++ src/translations/i18n.js
@@ -3,6 +3,10 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
  
 import { TRANSLATIONS_EN } from "./en/translations";
+import { TRANSLATIONS_FR } from "./fr/translations";
+import { TRANSLATIONS_ES } from "./es/translations";
+import { TRANSLATIONS_DE } from "./de/translations";
+import { TRANSLATIONS_ZH } from "./zh/translations";
  
 i18n
   .use(LanguageDetector)
@@ -11,9 +15,21 @@ i18n
      resources: {
         en: {
           translation: TRANSLATIONS_EN
-        }
+        },
+        fr: {
+          translation: TRANSLATIONS_FR
+        },
+        es: {
+          translation: TRANSLATIONS_ES
+        },
+        de: {
+          translation: TRANSLATIONS_DE
+        },
+        zh: {
+          translation: TRANSLATIONS_ZH
+        },
      },
-     lng: "en",
+     lng: "zh",
      interpolation: {
        escapeValue: false // react already safes from xss
      }
