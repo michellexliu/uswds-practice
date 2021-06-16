f3fbecd3811743d8c9bf0a01c23e2752649b62a7
Michelle <mliuu3259@gmail.com>
2021-06-16 16:29:04 -0400

    set language buttons to changeLanguage when clicked— still need to figure out how to persist this when user changes pages

--- src/components/LangSelect.js
+++ src/components/LangSelect.js
@@ -1,14 +1,21 @@
-import { Link, NavList, GridContainer } from '@trussworks/react-uswds';
+import { Button, NavList, GridContainer } from '@trussworks/react-uswds';
 import '@trussworks/react-uswds/lib/index.css';
+import { useTranslation } from "react-i18next";
 
 import { LANGS } from '../lib/constants';
 
-const listItems = LANGS.map((lang) => {
-  // TODO: Update hrefs after figuring out language routing situation— potentially might need equivalent of onclick instead
-  return <Link href="" key={lang.code}>{lang.name}</Link>
-})
-
 function LangSelect() {
+  const { i18n } = useTranslation();
+  
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
+  const listItems = LANGS.map((lang) => {
+    // TODO: Update hrefs after figuring out language routing situation— potentially might need equivalent of onclick instead
+    return <Button onClick={() => changeLanguage(lang.code)} key={lang.code}>{lang.name}</Button>
+  })
+
   return (
     <GridContainer containerSize="widescreen">
       <NavList items={listItems} type="primary"/>
