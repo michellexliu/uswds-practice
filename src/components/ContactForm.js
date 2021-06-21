314b1f45c57877594eaafcd9cf3f245538e3f061
Michelle <mliuu3259@gmail.com>
2021-06-21 11:23:23 -0400

    updated README

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -11,13 +11,13 @@ function ContactForm() {
     <Form className={i18n.language === "ar" ? "rtlText" : ""}>
       <FormGroup>
         <Label htmlFor="full-name" className={i18n.language === "ar" ? "rtl rtlText" : ""}>{t('full-name')}</Label>
-        <TextInput id="full-name" name="full-name" type="text" className={i18n.language === "ar" ? "rtl rtlText" : ""} />
+        <TextInput id="full-name" name="full-name" type="text" className={i18n.language === "ar" ? "rtl rtlText" : ""} autoComplete="name" />
         <Label htmlFor="subject" className={i18n.language === "ar" ? "rtl rtlText" : ""}>{t('subject')}</Label>
-        <TextInput id="subject" name="subject" type="text" className={i18n.language === "ar" ? "rtl rtlText" : ""} />
+        <TextInput id="subject" name="subject" type="text" className={i18n.language === "ar" ? "rtl rtlText" : ""} autoComplete="subject" />
         <Label htmlFor="email" className={i18n.language === "ar" ? "rtl rtlText" : ""}>{t('email')}</Label>
-        <TextInput id="email" name="email" type="email"  className={i18n.language === "ar" ? "rtl rtlText" : ""}/>
+        <TextInput id="email" name="email" type="email"  className={i18n.language === "ar" ? "rtl rtlText" : ""} autoComplete="email"/>
         <Label htmlFor="message" className={i18n.language === "ar" ? "rtl rtlText" : ""}>{t('message')}</Label>
-        <Textarea id="message" name="message" className={i18n.language === "ar" ? "rtl rtlText" : ""} />
+        <Textarea id="message" name="message" className={i18n.language === "ar" ? "rtl rtlText" : ""} autoComplete="message" />
         <Button type="submit" aria-label="Submit">{t("submit")}</Button>
       </FormGroup>
     </Form>
